bccce60d6f866812625085e533a4eeb261494fd5
Ekrem <ekremteke.uk@gmail.com>
2025-09-07 14:18:04 +0100

    sunject was added

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -13,7 +13,7 @@ export const actions: Actions = {
             return fail(400, { error: 'Please fill in all required fields.' });
         }
 
-      const powerAutomateUrl = 'https://defaultb3946708d7484dcf963d354fb648b4.d2.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/49810e61a162491c9ca617045ed4819a/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=71heziHMgnq9c4gUShmqLAVeBQC5XIdhZ2J_KILL3Jo'; // Flow URL'ni buraya ekle
+      const powerAutomateUrl = 'https://defaultb3946708d7484dcf963d354fb648b4.d2.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/49810e61a162491c9ca617045ed4819a/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=71heziHMgnq9c4gUShmqLAVeBQC5XIdhZ2J_KILL3Jo'; // Flow URL
 
 
         const response = await fetch(powerAutomateUrl, {
